d89e432031054ec613c09a06c8ff734fade298bd
VitoRodri <115217659+VitoRodri@users.noreply.github.com>
2023-01-16 02:38:16 +0000

    correvted the velocity

--- prototype.py
+++ prototype.py
@@ -17,19 +17,23 @@ x1=1010
 x2=1020
 v0=0
 
+
 def background():
-        
+      
     pygame.draw.polygon(screen,(230,230,230),(p0,p1,p3,p2),0)
     pygame.draw.polygon(screen,(40,40,200),(p2,(1000,380),(1000,340),(200,340)),0)
     pygame.draw.polygon(screen,(40,180,40),((100,300),(150,300),(150,280),(100,280)),0)
+    pygame.draw.polygon(screen,(40,180,40),((125,280),(140,280),(140,250),(125,250)),0)
+
     
+
 
 def shoot(x1,x2):
     
     FPS2=30
-    x0=100
+    x0=80
     y0=300
-    x3=100
+    x3=80
     y3=300
 
     mouse=pygame.mouse.get_pos()
@@ -81,24 +85,26 @@ while True:
         elif event.type==pygame.MOUSEBUTTONUP:
                 mouse=False
                 shoot(x1,x2)
+  
 
 
     if mouse==True and v0<90:
-        v0=v0+1
-        print(v0)
+        v0=v0+5
+        
     elif mouse==True and v0==90:
         pass
     else:
         v0=0
 
     
-        
+  
                     
     background()
     x1=x1-1
     x2=x2-1
 
     pygame.draw.polygon(screen,(150,40,40),((x1,340),(x2,340),(x2,330),(x1,330)),0)
+    
 
     pygame.display.update()
     fpsClock.tick(FPS)
